ff86075e972cb547d93c2a128a1a5642a4c84757
abe <abestbet1@gmail.com>
2017-05-23 10:33:30 -0700

    app.modules

---
+++ app/core/modules/app.modules.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict'
+
+  angular
+    .module('TutorApp', ['TutorApp.Controllers',
+                      'TutorApp.Services',
+                      'TutorApp.Configs',
+                      'YogaApp.Constants',
+                      ])
+
+    .run(function() {
+      
+    })
+      
+})();
