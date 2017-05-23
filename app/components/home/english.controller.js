3f94deec183f96d0fec182659ba3631a1311f345
abe <abestbet1@gmail.com>
2017-05-23 11:10:24 -0700

    change controller name to English controller

---
+++ app/components/home/english.controller.js
@@ -0,0 +1,19 @@
+(function(module) {
+
+  "use strict";
+  var EnglishCtrl;
+  EnglishCtrl.$inject = [];
+
+
+
+  function EnglishCtrl() {
+    
+   console.log('home running')
+   var vm = this;
+   
+  }
+
+
+  module.controller('EnglishCtrl', EnglishCtrl);
+
+})(angular.module('TutorApp.Controllers'));
